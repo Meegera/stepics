37ab10a1dac840ef8c7d92a4667c970c17fb5a17
Гера <gretadonskova@gmail.com>
2023-02-24 05:16:17 +0300

    step_3_3_4

---
+++ src/step_3_3_4/Main.java
@@ -0,0 +1,28 @@
+package step_3_3_4;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    public static void main(String[] args) {
+
+        new MyFrame();
+    }
+
+}
+
+class MyFrame extends JFrame {
+
+    public MyFrame() {
+
+        MyPanel panel = new MyPanel();
+        this.setSize(800, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Простое перемещение");
+        this.add(panel);
+        this.pack();
+        this.setVisible(true);
+
+    }
+}
+
